68f289f01f82e18a5b8659e813d71dcaaab1883a
Gyeongjae Choi <gyeongjae@cloudflare.com>
2026-03-25 09:33:08 +0000

    Replace the python package URL to R2

--- src/workerd/server/tests/python/sdk/proxy.js
+++ src/workerd/server/tests/python/sdk/proxy.js
@@ -4,12 +4,7 @@
 export default {
   async fetch(req, env) {
     const url = new URL(req.url);
-    if (
-      url.hostname == 'python-packages.edgeworker.net' ||
-      url.hostname == 'pyodide-packages.runtime-playground.workers.dev'
-    ) {
-      return env.INTERNET.fetch(req);
-    } else if (url.hostname == 'example.com') {
+    if (url.hostname == 'example.com') {
       return env.PYTHON.fetch(req);
     }
 
